9a4650eeb5328c756bb65bf0500ea24023154de6
Wojtek <wojciechopitz@gmail.com>
2025-01-16 12:06:16 +0100

    Added navigation config, replaced <a> with <Link>

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,19 +1,15 @@
+import { Link } from "react-router-dom";
+import { menuConfig } from "../../config/menu";
 import styles from "./Navigation.module.css";
 
 export const Navigation = () => {
   return (
-    <nav className={styles.navlist}>
-      <ul>
-        <li>
-          <a href="/">Homepage</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-        <li>
-          <a href="/about-us">About us</a>
-        </li>
-      </ul>
+    <nav className={styles.navContainer}>
+      {menuConfig.map((item) => (
+        <Link to={item.url} key={item.url} className={styles.link}>
+          {item.label}
+        </Link>
+      ))}
     </nav>
   );
 };
---
+++ src/config/menu.js
@@ -0,0 +1,5 @@
+export const menuConfig = [
+  { url: "/", label: "Homepage" },
+  { url: "/contact", label: "Contact" },
+  { url: "/about-us", label: "About-us" },
+];
